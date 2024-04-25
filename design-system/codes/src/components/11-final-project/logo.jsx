fb95bdb3dc7241a5c8bd8e17746d8f264a2e4cc4
ngocla99 <ngoc.leanh1999@gmail.com>
2024-04-25 13:58:36 +0700

    chore: remove code

--- design-system/codes/src/components/11-final-project/logo.jsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Logo = styled.div`
-  --size: ${(props) => props.size};
-  background: ${(props) => (props.inverse ? "#0F1623" : "white")};
-  width: var(--size);
-  height: var(--size);
-  ${(props) => !props.square && "border-radius: 50%;"}
-`;
--- design-system/codes/src/components/9-media-werapper-pattern/components.jsx
+++
@@ -1,9 +0,0 @@
-import { Pad } from "../7-pad-pattern/start";
-import { Center } from "../8-center-pattern/start";
-
-
-export const Description = (props) => (
-  <Pad padding="l">
-    <Center centerText>{props.children}</Center>
-  </Pad>
-);
